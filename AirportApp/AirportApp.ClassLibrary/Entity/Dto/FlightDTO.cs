6b21c8d3c03e973401e6d7c6d353ead631c7526b
KunMihai <mihaikun2501@gmail.com>
2026-05-31 14:23:01 +0300

    feat: update FlightDTO (runwayId), RouteDTO (DateOnly/TimeOnly), AirportDTO (name)

--- AirportApp/AirportApp.ClassLibrary/Entity/Dto/FlightDTO.cs
+++ AirportApp/AirportApp.ClassLibrary/Entity/Dto/FlightDTO.cs
@@ -2,5 +2,12 @@ using System;
 
 namespace AirportApp.ClassLibrary.Entity.Dto
 {
-    public record FlightDTO(int id, int routeId, int gateId, DateTime date, string flightNumber, RouteDTO? route);
+    public record FlightDTO(
+        int id,
+        int routeId,
+        int gateId,
+        int runwayId,
+        DateTime date,
+        string flightNumber,
+        RouteDTO? route);
 }
