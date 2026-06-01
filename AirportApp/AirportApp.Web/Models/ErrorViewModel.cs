4a3c52402c0881b78212810f2707c86d424a759e
AndreeaJigarov <andreea.jigarov@stud.ubbcluj.ro>
2026-06-01 18:48:15 +0300

    commit

---
+++ AirportApp/AirportApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirportApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
